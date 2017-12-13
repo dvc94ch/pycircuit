806853108fc74b15ff4d862a0cd8f1baaeadf01f
David Craven <david@craven.ch>
2017-12-13 16:32:21 +0100

    viewer: Pass net and pcb paths as args.

--- viewer/app.js
+++ viewer/app.js
@@ -2,14 +2,27 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use('/css', express.static('css'));
-app.use('/js', express.static('js'));
-app.use(express.static('files'));
+var index = path.join(__dirname, 'index.html');
+var net = path.join(process.cwd(), process.argv[2]);
+var pcb = path.join(process.cwd(), process.argv[3]);
+
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(index);
+});
+
+app.get('/net', function(req, res) {
+    res.sendFile(net);
+});
+
+app.get('/pcb', function(req, res) {
+    res.sendFile(pcb);
 });
 
 app.listen(3000, function () {
     console.log('Viewer listening on port 3000!');
+    console.log('net file:', net);
+    console.log('pcb file:', pcb);
 });
