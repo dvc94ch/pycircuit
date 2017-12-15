5a29d0aa6bb26f07ff31eb758c5899d37dc75af1
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-12-15 14:51:20 +0000

    Joule thief: align circuit connections for readability

--- examples/joule_thief/joule_thief.py
+++ examples/joule_thief/joule_thief.py
@@ -15,15 +15,18 @@ Footprint('R0805', 'R', '0805',
           Map(1, '1'),
           Map(2, '2'))
 
+
 Footprint('D0805', 'D', '0805',
           Map(1, 'A'),
           Map(2, 'K'))
+
 
 for a, b, c in ['BCE', 'BEC', 'CBE', 'CEB', 'ECB', 'EBC']:
     Footprint('SOT23' + a + b + c, 'Q', 'SOT23',
               Map(1, a),
               Map(2, b),
               Map(3, c))
+
 
 @circuit('TOP')
 def top():
@@ -32,14 +35,16 @@ def top():
     Node('R1', 'R')
     Node('Q1', 'Q')
     Node('LED1', 'D')
-    Ref('BAT1')['V'] + Net('VCC') + Ref('R1')['1']
-    Ref('R1')['2'] + Ref('L1')['1']
-    Net('VCC') + Ref('L1')['2']
-    Ref('L1')['3'] + Ref('Q1')['B']
-    Ref('L1')['4'] + Net('n1') + Ref('Q1')['C']
-    Net('n1') + Ref('LED1')['A']
-    Ref('Q1')['E'] + Net('GND')
-    Ref('LED1')['K'] + Net('GND')
+    Ref('BAT1')['V']   + Net('VCC')       + Ref('R1')['1']
+    Ref('BAT1')['GND'] + Net('GND')
+    Ref('R1')['2']     + Ref('L1')['1']
+    Net('VCC')         + Ref('L1')['2']
+    Ref('L1')['3']     + Ref('Q1')['B']
+    Ref('L1')['4']     + Net('n1')        + Ref('Q1')['C']
+    Ref('Q1')['E']     + Net('GND')
+    Net('n1')          + Ref('LED1')['A']
+    Ref('LED1')['K']   + Net('GND')
+
 
 if __name__ == '__main__':
     pcb = Pcb.oshpark_4layer(top())
