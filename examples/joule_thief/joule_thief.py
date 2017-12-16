7255ac0dd0c7acc72252c30577d08e21e59397d4
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-12-16 16:52:49 +0000

    Joule thief: clean up imports and names

--- examples/joule_thief/joule_thief.py
+++ examples/joule_thief/joule_thief.py
@@ -1,15 +1,12 @@
-from pycircuit.device import *
-from pycircuit.circuit import *
-from pycircuit.footprint import *
-from pycircuit.package import *
-from pycircuit.pcb import *
-from pycircuit.formats import *
-from pycircuit.library import *
-from pycircuit.library.connectors import *
-from pykicad.module import Module
-from pykicad.pcb import Zone
+from pycircuit.device import Device, Pin
+from pycircuit.circuit import circuit, Node, Ref, Net
+from pycircuit.footprint import Footprint, Map
+from pycircuit.package import Package, RectCrtyd, DualPads
+from pycircuit.pcb import Pcb
+from pycircuit.formats import * #extends Pcb and Node
+from pycircuit.library.connectors import * #defines some Devices
+from pykicad.pcb import Zone #extends Pcb
 
-Device('Choke', pins=(Pin('1'), Pin('2'), Pin('3'), Pin('4')))
 
 Footprint('R0805', 'R', '0805',
           Map(1, '1'),
@@ -25,34 +22,38 @@ Footprint('BAT0805', 'DCCONN', '0805',
           Map(1, 'V'),
           Map(2, 'GND'))
 
-for a, b, c in ['BCE', 'BEC', 'CBE', 'CEB', 'ECB', 'EBC']:
+
+for a, b, c in ('BCE', 'BEC', 'CBE', 'CEB', 'ECB', 'EBC'):
     Footprint('SOT23' + a + b + c, 'Q', 'SOT23',
               Map(1, a),
               Map(2, b),
               Map(3, c))
 
 
-Package('TDK:ACT45B', RectCrtyd(5.9, 3.4), DualPads(4, 2.5, radius=2.275),
+Device('Transformer_1P_1S', pins=(Pin('1'), Pin('2'), Pin('3'), Pin('4')))
+
+
+Package('TDK ACT45B', RectCrtyd(5.9, 3.4), DualPads(4, 2.5, radius=2.275),
         package_size=(5.9, 3.4), pad_size=(0.9, 1.35))
 
 
-Footprint('TDK:ACT45B', 'Choke', 'TDK:ACT45B',
+Footprint('TDK ACT45B', 'Transformer_1P_1S', 'TDK ACT45B',
           Map(1, '1'), Map(2, '2'), Map(3, '4'), Map(4, '3'))
 
 
 @circuit('TOP')
 def top():
-    Node('L1', 'Choke')
+    Node('TR1', 'Transformer_1P_1S')
     Node('BAT1', 'DCCONN')
     Node('R1', 'R')
     Node('Q1', 'Q')
     Node('LED1', 'D')
     Ref('BAT1')['V']   + Net('VCC') + Ref('R1')['1']
     Ref('BAT1')['GND'] + Net('GND')
-    Ref('R1')['2']     + Net('n1')  + Ref('L1')['1']
-    Ref('L1')['2']     + Net('VCC')
-    Ref('L1')['3']     + Net('n2')  + Ref('Q1')['B']
-    Ref('L1')['4']     + Net('n3')  + Ref('Q1')['C']
+    Ref('R1')['2']     + Net('n1')  + Ref('TR1')['1']
+    Ref('TR1')['2']    + Net('VCC')
+    Ref('TR1')['3']    + Net('n2')  + Ref('Q1')['B']
+    Ref('TR1')['4']    + Net('n3')  + Ref('Q1')['C']
     Ref('Q1')['E']     + Net('GND')
     Ref('LED1')['A']   + Net('n3')
     Ref('LED1')['K']   + Net('GND')
