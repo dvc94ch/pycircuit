e6f66340518312d894f6c2e01ca93a233e66dc37
David Craven <david@craven.ch>
2018-01-02 03:29:38 +0100

    viewer: Add default arguments.

--- viewer/app.js
+++ viewer/app.js
@@ -2,10 +2,14 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+function random_port() {
+    return Math.floor(Math.random() * 100) + 3000;
+}
+
 var index = path.join(__dirname, 'index.html');
-var port = process.argv[2];
-var net = path.join(process.cwd(), process.argv[3]);
-var pcb = path.join(process.cwd(), process.argv[4]);
+var port = process.argv[2] || random_port();
+var net = path.join(process.cwd(), process.argv[3] || path.join('build', 'net.dot.svg'));
+var pcb = path.join(process.cwd(), process.argv[4] || path.join('build', 'pcb.svg'));
 
 app.use('/css', express.static(path.join(__dirname, 'css')));
 app.use('/js', express.static(path.join(__dirname, 'js')));
